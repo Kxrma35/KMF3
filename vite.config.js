dad671fdecdfd244e0b3ad053967e8b59301edfc
kxrma35 <karmanjeruh5@gmail.com>
2026-04-13 10:00:17 +0300

    replacing the base on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bulking-tracker/',
   plugins: [react()],
+  base: '/bulking-tracker/'   
 })
